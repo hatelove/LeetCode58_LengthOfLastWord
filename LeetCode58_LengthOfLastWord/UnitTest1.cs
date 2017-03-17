12f6f7f2168e7240d63301e91fe1dd9190cfadc5
hatelove <jay.hatelove@gmail.com>
2017-03-17 15:27:13 +0800

    add first test case: s is "bc" should return 2

---
+++ LeetCode58_LengthOfLastWord/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode58_LengthOfLastWord
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void s_is_bc_should_return_2()
+        {
+            var s = "bc";
+            Assert.AreEqual(2, new Solution().LengthOfLastWord(s));
+        }
+    }
+
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
